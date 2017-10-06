f3c86dae8b9e1d649e9ccfc98d117d53d220263e
hechfer <ing.luis.fernando@outlook.com>
2017-10-06 04:51:54 -0500

    Delete SequenceIdDao.java

--- JsenLogica/src/main/java/com/mx/jsen/application/dao/SequenceIdDao.java
+++
@@ -1,7 +0,0 @@
-package com.mx.jsen.application.dao;
-
-import com.mx.jsen.application.exception.SequenceException;
-
-public interface SequenceIdDao {
-	public Long getNextSequenceId(String key) throws SequenceException;
-}
